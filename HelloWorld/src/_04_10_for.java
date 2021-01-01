b020a83967b7e6087121171bdd4a980d400d52d0
chloe <seolahchloe.chung@gmail.com>
2021-01-01 19:29:54 +0900

    Add Chapter4 class10 For for study java

---
+++ HelloWorld/src/_04_10_for.java
@@ -0,0 +1,10 @@
+public class _04_10_for {
+
+    public static void main(String[] args) {
+        int sum = 0;
+        for (int i = 1; i <= 5; i++) {
+            sum += 1;
+        }
+        System.out.println(sum);
+    }
+}
