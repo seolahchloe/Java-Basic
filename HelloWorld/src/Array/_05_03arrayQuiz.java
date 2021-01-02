3a08ae7e7e64a6bee7d8e5b08409a67d658c82fc
chloe <seolahchloe.chung@gmail.com>
2021-01-02 16:27:32 +0900

    Update re-arraged code

--- HelloWorld/src/Array/_05_03arrayQuiz.java
+++ HelloWorld/src/Array/_05_03arrayQuiz.java
@@ -1,4 +1,16 @@
 package Array;
 
 public class _05_03arrayQuiz {
+    public static void main(String[] args) {
+        int[] intArray = new int[30];
+        for (int i = 0; i < intArray.length; i++) {
+            intArray[i] = 1001 + i;
+        }
+
+        String[] remainders = {"Zero", "One", "Two", "Three"};
+        for (int element : intArray) {
+            System.out.println(remainders[element % 4]);
+        }
+
+    }
 }
