946d4830ad21deab1cc4cacdc331cb892072e7c4
chloe <seolahchloe.chung@gmail.com>
2021-01-17 23:35:52 +0900

    Add Source Code of ShapePrinter Quiz

---
+++ HelloWorld/src/ShapePrinter/ShapePrinterDriver.java
@@ -0,0 +1,14 @@
+package ShapePrinter;
+
+public class ShapePrinterDriver {
+    public static void main(String[] args) {
+        ShapePrinter printer = new ShapePrinter();
+
+        // 테스트
+        printer.printTriangle(3);
+        System.out.println("----------");
+        printer.printTriangle(5);
+        System.out.println("----------");
+        printer.printTriangle(10);
+    }
+}
