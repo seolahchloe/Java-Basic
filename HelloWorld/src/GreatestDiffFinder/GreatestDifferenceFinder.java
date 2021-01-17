643dc1bc000cf4e9a06c0a3567f4bf0bb188bc93
chloe <seolahchloe.chung@gmail.com>
2021-01-17 23:19:18 +0900

    Add Source Code of GreatestDifferenceFinder Quiz

--- HelloWorld/src/GreatestDiffFinder/GreatestDifferenceFinder.java
+++ HelloWorld/src/GreatestDiffFinder/GreatestDifferenceFinder.java
@@ -1,2 +1,21 @@
-package GreatestDiffFinder;public class GreatestDifferenceFinder {
-}
+package GreatestDiffFinder;
+
+public class GreatestDifferenceFinder {
+    int greatestDifference(int[] intArray) {
+        if (intArray.length < 2) {
+            return 0;
+        }
+        int max = intArray[0];
+        int min = intArray[0];
+
+        for (int i = 0; i < intArray.length; i++) {
+            if (max < intArray[i]) {
+                max = intArray[i];
+            }
+            if (min > intArray[i]) {
+                min = intArray[i];
+            }
+
+            } return max - min;
+        }
+    }
